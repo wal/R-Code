9993414dacf1de33c8eec2fdabcf1b18bd6f956e
Wal McConnell <walter.mcconnell@gmail.com>
2018-07-26 11:43:37 +0100

    update process script

---
+++ process/import_eda.R
@@ -0,0 +1,74 @@
+                                        # WIP
+## Questions
+
+## Assumptions
+
+
+
+                                        # QUESTION
+## What specifically is the research objective / question
+
+## Why? - Do you understand the scientific / business application ?
+
+## What type of analysis is required ?
+### Descriptive, Exploratory, Inferential, Predictive, Causal, Mechanistic
+
+## What is the objective for successful project ? 
+## What is the metric for success ?
+
+                                        # DATA
+## What / Where is the data ?
+### What kind of data is it ? (tabular, text, binary ?)
+
+## How was the data collected / Who collected it ?
+#### Is it sampled / complete ?
+
+## Is there a code book / description of the data anywhere ?
+
+## Is it possible to answer the question using this data ?
+
+                                        # IMPORT DATA
+## How to read in the data ? (Direct / Indirect )
+
+
+## What are the dimensions of the data ?
+
+## Create a variables table (Name, type, #unique_values, #missing, %missing)
+
+## Is the data tidy ?
+
+
+                                        # EDA
+## NUMERIC VARIABLES
+
+### Missing Data
+### Correlation to Target Variable
+#### Per Variable EDA (in order of correlation)
+##### Description & Unit
+##### Distribution v Target Variable
+###### Intuitive, Outliers, Errors, Patterns ?
+##### Transformations
+###### Binnable ?
+
+
+## Non Numeric Data
+### Missing Data
+### Per Variable EDA
+##### Description & Unit
+##### Identify Type & code
+##### Distribution v Target Variable
+###### Intuitive, Outliers, Errors, Patterns ?
+##### Transformations
+###### Reduce number of Bins
+
+## Bivariate EDA
+## Multivariate EDA
+
+## Dimensionality Reduction
+
+                                        # Inference
+
+                                        # Prediction
+
+                                        # Reporting
+
